a13b204c79c82e6a4a8518988ac25964a2e6e354
Sahil <sahil22undale@gmail.com>
2026-04-20 17:09:28 +0530

    Update components and settings

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -6,6 +6,7 @@ import { ReactNode } from "react";
 export function PageTransition({ children }: { children: ReactNode }) {
   return (
     <motion.div
+      className="relative z-10"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.25 }}
