725f519bf2ee1918544070c77775192c978b2dba
sudosahil <sahil22undale@gmail.com>
2026-06-01 18:32:53 +0530

    Fix failing Vercel build: drop unused Link import in my-projects page

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -1,5 +1,4 @@
-﻿import Link from "next/link";
-import { Reveal, RevealLines } from "@/components/Reveal";
+﻿import { Reveal, RevealLines } from "@/components/Reveal";
 import { SectionLabel } from "@/components/SectionLabel";
 import { CaseCard } from "@/components/CaseCard";
 import { caseProjects } from "@/lib/content";
